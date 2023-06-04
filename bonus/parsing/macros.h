8c2afee38e9e54fde0e80c69431c347b390a86bd
Wissam Essaiydy <wiessaiy@e1r8p10.1337.ma>
2023-06-04 04:48:15 +0100

    OKay

--- bonus/parsing/macros.h
+++ bonus/parsing/macros.h
@@ -11,24 +11,12 @@
 /* ************************************************************************** */
 
 #ifndef MACROS_H
-#define MACROS_H
+# define MACROS_H
 
- 
-
-
-  
-    
-
-
-
-#define         NAME_ERROR      "Incorrect file name \033[0m"
-#define         FILE_NOT_FOUND  "file not found \033[0m"
-#define         LMAP            "New Line In Map\033[0m"
-#define         ERROR           "\033[91;1mError \n"
-#define         WRONG_ARGS	  "Wrong number of arguments\033[0m"
- 
-
-
- 
+# define NAME_ERROR "Incorrect file name \033[0m"
+# define FILE_NOT_FOUND "file not found \033[0m"
+# define LMAP "New Line In Map\033[0m"
+# define ERROR "\033[91;1mError \n"
+# define WRONG_ARGS "Wrong number of arguments\033[0m"
 
 #endif
